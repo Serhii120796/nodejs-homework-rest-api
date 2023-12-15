6b4cc4616b949baa3e0e12f6d72fcc333bfe83bb
Serhii120796 <sfedyura@ukr.net>
2023-12-15 20:17:18 +0200

    Update contacts-schemas.js

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
-const message = { "any.required": "missing required field: {{#label}}" };
+const message = { "any.required": "missing required field" };
 
 export const contactAddSchema = Joi.object({
-  name: Joi.string().required().messages(message),
-  email: Joi.string().required().messages(message),
-  phone: Joi.number().min(10).required().messages(message),
+  name: Joi.string().required().messages({ "any.required": "missing required name field" }),
+  email: Joi.string().required().messages({ "any.required": "missing required email field" }),
+  phone: Joi.number().min(10).required().messages({ "any.required": "missing required phone field" }),
 });
 
 export const contactUpdateSchema = Joi.object({
